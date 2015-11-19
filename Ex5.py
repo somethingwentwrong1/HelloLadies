b1e3bca73501eb21cf6172b34ca05de29dd86f7b
[Jonny Choi] <jonnychoi@seoulchristian.org>
2015-11-19 15:13:14 +0900

    Ex6.py

--- Ex5.py
+++ Ex5.py
@@ -10,7 +10,7 @@ print "Let's talk about %s." % my_name
 print "He's %d inches tall."%my_height
 print "He's %d pounds heavy." % my_weight
 print"Actually that's not too heavy."
-print"He's got %s eyes and %s hair."%(my_eyes, my_hair)
+print"He's got %s eyes and % hair."%(my_eyes, my_hair)
 print"His teeth are usually %s depending on the coffee." % my_teeth
 
 #this line is tricky, try to get it exactly right
