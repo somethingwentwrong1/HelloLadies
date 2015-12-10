fbfcdfd7048cac079256ecea625e1e2816e0c3f6
[Jonny Choi] <jonnychoi@seoulchristian.org>
2015-12-10 15:27:35 +0900

    ex15.py

--- ex14.py
+++ ex14.py
@@ -6,7 +6,7 @@ prompt = '> '
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
-liles = raw_input(prompt)
+likes = raw_input(prompt)
 
 print "Where do you live %s?" % user_name
 lives= raw_input(prompt)
